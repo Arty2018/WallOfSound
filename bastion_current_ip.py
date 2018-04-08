53384bf1d277dd07959f0edd6aef30d1ce0d786f
Arty2018 <lukeroberta1@yahoo.com>
2018-04-08 13:51:28 -0700

    Revised script to accept command line options

--- bastion_current_ip.py
+++ bastion_current_ip.py
@@ -1,70 +1,74 @@
 # obtain current public ip, create a variables.tfvars file 
-# with this ip as the user_ip (along with other vars prompted
-# from user). 
+# with this ip as user_ip (along with other vars obtained
+# from command line input). 
 # Run terraform shell script to apply the instance
 
 import os,subprocess
+import argparse
+
+
 
 def main():
 
-	myip=get_public_ip()
-	ssh_key_name,subnet_list,name,zone_name,region = get_variables()
+	parser = argparse.ArgumentParser(description="Pass Terraform variables for bastion")
 
-	# create or overwrite variables.tfcars with user input from get_variables function
+	parser.add_argument('-k','--ssh_key_name', type=str)
+	parser.add_argument('-s','--subnet_list', type=str)
+	parser.add_argument('-n','--name',type=str)
+	parser.add_argument('-z','--zone_name',type=str)
+	parser.add_argument('-r','--region',type=str)
+	parser.add_argument('--destroy',action='store_true',default=False)
+	parser.add_argument('--plan_destroy',action='store_true',default=False)
+	parser.add_argument('--plan_apply',action='store_true', default=False)
+	parser.add_argument('--apply',action='store_true',default=False)
 
-	file = open("variables.tfvars","w")
-	file.write("ssh_key_name = " + "\"" + str(ssh_key_name) + "\"\n")
-	file.write("subnet_list = " + "\"" + str(subnet_list) + "\"\n")
-	file.write("user_ip = " + "\"" + myip + "\"\n")
-	file.write("name = " + "\"" + str(name) + "\"\n")
-	file.write("zone_name = " + "\"" + str(zone_name) + "\"\n")
-	file.write("region = " + "\"" + str(region) + "\"\n")
-	file.close()
+	args = parser.parse_args()
 
-	run_bastion_tf()
+	if args.plan_destroy:
+		os.system('./plan_destroy_bastion.sh')
+		return
 
-def get_public_ip():
-	myip = subprocess.check_output(["dig","+short","myip.opendns.com","@resolver1.opendns.com"])
+	if args.destroy:
+		os.system('./destroy_bastion.sh')
+		return
+
+	if args.plan_apply | args.apply:
+		myip=get_public_ip()
+		# create or overwrite variables.tfvars with user input from command line parser
+
+		file = open("variables.tfvars","w")
+		file.write("ssh_key_name = " + "\"" + args.ssh_key_name + "\"\n")
+		file.write("subnet_list = " + "\"" + args.subnet_list + "\"\n")
+		file.write("user_ip = " + "\"" + myip + "\"\n")
+		file.write("name = " + "\"" + args.name + "\"\n")
+		file.write("zone_name = " + "\"" + args.zone_name + "\"\n")
+		file.write("region = " + "\"" + args.region + "\"\n")
+		file.close()
+
+		if args.plan_apply:
+			os.system('./plan_apply_bastion.sh')
+			return
+
+		if args.apply:
+			os.system('./apply_bastion.sh')
+			return
+
+
+def get_public_ip(): #attempt to obtain public ip. tries 2 different websites
+	try:
+		myip = subprocess.check_output(["dig","+short","myip.opendns.com","@resolver1.opendns.com"])
+	except:
+		try:
+			myip = subprocess.check_output(["curl", "-s","checkip.dyndns.org"])
+			myip = myip.split(":",1)[-1]
+			myip = myip.split("<",1)[0]
+		except:  
+			myip = raw_input('Error obtaining IP address. Please specify IP: ')
+ 	myip = myip.lstrip()
 	myip = myip.rstrip()
 	print ("Public IP obtained")
 	return str(myip.decode("utf-8"))
 
-def get_variables():
-	ssh_key_name= input('Enter ssh key file name: ')
-	subnet_list= input('Enter subnet list: ')
-	name = input('Enter A record: ')
-	zone_name = input('Enter domain name: ')
-	region = input('Enter AWS region: ')
-
-	return(ssh_key_name,subnet_list,name,zone_name,region)
-
-def run_bastion_tf():
-	print("\n")
-
-	while True:
-		action = input('Choose one of the followng: \n - plan_apply \n - plan_destroy \n - apply \n - destroy \n  (type "C" to cancel) \n')
-		if action not in ['plan_apply','plan_destroy','apply','destroy','C']:
-			continue
-		else:
-			break
-
-	print ("\n")
-
-	if action != "C":
-		print ("Running terraform " + action + " script...")
-	else:
-		print ("Quit program")
-
-	if action == "plan_apply":
-		os.system('./plan_apply_bastion.sh')
-	elif action == "plan_destroy":
-		os.system('./plan_destroy_bastion.sh')
-	elif action == "apply":
-		os.system('./apply_bastion.sh')
-	elif action == "destroy":
-		os.system('./destroy_bastion.sh')
-	elif action == "C":
-		return()
 
 	
 
